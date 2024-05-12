89701709fafb1215c1b21441470e3b0214c3c37a
Sandun Rathsara <srathsara1@gmail.com>
2024-05-12 09:34:03 +0530

    Hasher implemented

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ app.use(raw())
 app.use(json())
 app.use(urlencoded({extended: true}))
 
-// ======================================== Cluster Management ========================================
-// ====================================== Connect to the Cluster ======================================
+// Cluster Management ================================================================================
+// Connect to the Cluster ============================================================================
 app.post('/connect', (req: Request, res: Response) => {
 
     const nodeRepo = new NodeRepository(NODE_TABLE_FILE_PATH(getNodeId()))
@@ -28,21 +28,21 @@ app.post('/connect', (req: Request, res: Response) => {
     return res.send({name: getNodeName(), id: getNodeId(), role: getNodeRole()})
 })
 
-// ==================================== Disconnect from the Cluster ===================================
+// Disconnect from the Cluster =======================================================================
 app.post('/disconnect', (req: Request, res: Response) => {
     const nodeRepo = new NodeRepository(NODE_TABLE_FILE_PATH(getNodeId()))
     nodeRepo.removeNodeById(req.body.id);
     return res.send({success: true, message: 'Successfully disconnected from the cluster'});
 })
 
-// ================================== Check if the nodes are active ===================================
+// Check if the nodes are active =====================================================================
 app.get('/heart-beat', (_, res: Response) => {
     return res.send({id: getNodeId(), name: getNodeName(), role: getNodeRole()})
 })
 
 
-// ======================================== Storage Management ========================================
-// ========================================== Store Data ==============================================
+// Storage Management ================================================================================
+// Store Data ========================================================================================
 app.post('/store-data', async (req: Request, res: Response) => {
     if (getNodeRole() === ROLES.HASHER) {
         const data = await fetch(`http://localhost:300${calculateNodeId()}/hash-data`, {
@@ -62,7 +62,7 @@ app.post('/store-data', async (req: Request, res: Response) => {
     return res.status(201).send({success: true, message: 'Saved', data: {key}})
 })
 
-// ========================================== Get Data ==============================================
+// Get Data ========================================================================================
 app.get('/get-data', async (req: Request, res: Response) => {
     if (isEmpty(req.query.key)) return res.status(400).send({success: false, message: 'Bad Request'});
     const {key, internal} = req.query
@@ -76,7 +76,10 @@ app.get('/get-data', async (req: Request, res: Response) => {
     const data = dataRepo.getDataByKey(key as string)
 
     if (isEmpty(data)) {
-        if (parseInt(internal as string) === 1) return res.status(404).send({success: false, message: 'Data not found'});
+        if (parseInt(internal as string) === 1) return res.status(404).send({
+            success: false,
+            message: 'Data not found'
+        });
 
         const nodeIds = (new NodeRepository(NODE_TABLE_FILE_PATH(getNodeId())))
             .getAllNodes()
@@ -97,8 +100,8 @@ app.get('/get-data', async (req: Request, res: Response) => {
     return res.send({success: true, message: 'Success', data})
 })
 
-// ======================================== Hasher Management ========================================
-// ========================================== Hash Data ==============================================
+// Hasher Management ================================================================================
+// Hash Data ========================================================================================
 app.post('/hash-data', async (req: Request, res: Response) => {
     console.log('here', calculateNodeId())
     if (getNodeRole() === ROLES.RECEIVER) {
@@ -112,17 +115,26 @@ app.post('/hash-data', async (req: Request, res: Response) => {
     }
 
     const paragraph = req.body.paragraph
-    if (isEmpty(paragraph) || typeof paragraph !== 'string') return res.status(400).send({success: false, message: 'Bad Request'})
-
-    const algorithm = createHash('sha1')
-    const hash = algorithm.update(paragraph.substring(0, 10)).digest('hex')
-    const response = await fetch(`http://localhost:300${calculateNodeId()}/store-data`, {
-        method: 'POST',
-        body: JSON.stringify({key: hash, value: paragraph}),
-        headers: {'Content-Type': 'application/json'}
+    if (isEmpty(paragraph) || typeof paragraph !== 'string') return res.status(400).send({
+        success: false,
+        message: 'Bad Request'
     })
 
-    return res.send({success: true, message: 'Success', data: await response.json()})
+    const algorithm = createHash('sha1')
+    const key = algorithm.update(paragraph.substring(0, 10)).digest('hex')
+    const [res1, res2] = await Promise.all([fetch(`http://localhost:300${calculateNodeId()}/store-data`, {
+        method: 'POST',
+        body: JSON.stringify({key, value: paragraph}),
+        headers: {'Content-Type': 'application/json'}
+    }), fetch(`http://localhost:300${calculateNodeId()}/store-data`, {
+        method: 'POST',
+        body: JSON.stringify({key, value: paragraph}),
+        headers: {'Content-Type': 'application/json'}
+    })])
+
+    if (!res1.ok && !res2.ok) return res.status(500).send({success: false, message: 'Internal Server Error'})
+
+    return res.send({success: true, message: 'Success', data: {key}})
 })
 
 async function main() {
